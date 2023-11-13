b2fb92cc4138bdcb65eceb528b73028880e0a3f4
Devansh Agarwal <77046880+d852@users.noreply.github.com>
2023-11-13 20:56:31 +0530

    Add files via upload

---
+++ Cursor.js
@@ -0,0 +1,56 @@
+AFRAME.registerComponent('cursores', {
+    schema: {
+        selectedId:{default:"",type:"string"}
+    },
+
+    init: function () {
+        this.mouseEnter();
+        console.log("hi_hello1234")
+        this.mouseLeave();
+    },
+
+    update: function () {
+      // Do something when component's data is updated.
+    },
+    remove: function () {
+      // Do something the component or its entity is detached.
+    },
+
+    tick: function (time, timeDelta) {
+        
+    },
+
+    checkPlace:function(){
+        var id = this.el.getAttribute("id")
+        var placesId = ["budapest","taj_mahal","eiffel_tower","new_york_city"]
+        if(placesId.includes(id)){
+            var place = document.querySelector("#placeContainer")
+            place.setAttribute("cursores",{selectedId:id})
+            place.setAttribute("geometry",{primitive:"ring",radiusInner:0.9,radiusOuter:2})
+            place.setAttribute("material",{color:"white",opacity:1})
+            console.log(id)
+        }
+    },
+
+    mouseEnter:function(){
+        this.el.addEventListener("mouseenter",()=>{
+            this.checkPlace();
+            console.log("hello123")
+        })
+    },
+
+    mouseLeave:function(){
+        this.el.addEventListener("mouseleave",()=>{
+        var {element} = this.data
+        if(element){
+            var button = document.querySelector(`#${element}`)
+            var id = button.getAttribute("id")
+            if (id === element){
+                button.setAttribute("geometry",{primitive:"ring",radiusInner:0.9,radiusOuter:1})
+                button.setAttribute("material",{color:"black",opacity:1})
+            }
+        }
+        })
+    }
+
+});
--- Tour.js
+++ Tour.js
@@ -1,4 +1,4 @@
-AFRAME.registerComponent('Tour', {
+AFRAME.registerComponent('tour', {
     schema: {
         
     },
@@ -11,55 +11,67 @@ AFRAME.registerComponent('Tour', {
     createCards:function(){
         const cards = [
             {id:"budapest",
-            url:"",
-            tile:"Budapest"},
+            url:"./thumbnails/budapest.jpg",
+            title:"Budapest"},
             {id:"taj_mahal",
-            url:"",
-            tile:"Taj Mahal"},
+            url:"./thumbnails/taj_mahal.png",
+            title:"Taj Mahal"},
             {id:"eiffel_tower",
-            url:"",
+            url:"./thumbnails/eiffel_tower.jpg",
             title:"Eiffel Tower"},
             {id:"new_york_city",
-            url:"",
+            url:"./thumbnails/new_york_city.png",
             title:"New York City"}]
 
-        var position = -50
+        var position = -4
 
-        for(i in cards){
-            var posx=position + 25
-            var posy = 10
-            var posz = 10
+        for(i of cards){
+            var posx=position + 4
+            var posy = 2
+            var posz = -5
             
             var pos = {x:posx,y:posy,z:posz}
             position = posx
 
             var border = this.craeteBoundary(pos,i.id)
             var cardImage = this.createCardImage(i)
+            var title = this.createTitle(i,pos)
+            console.log(i.title);
             
+            cardImage.appendChild(title)
             border.appendChild(cardImage)
-            this.placesContainer.appendChild(border)
+            this.placeContainer.appendChild(border)
 
         }
+        
+        console.log("HEllo")
     },
 
     craeteBoundary:function(position,id){
         var element = document.createElement("a-entity")
         element.setAttribute("id",id)
         element.setAttribute("visible",true)
-        element.setAttribute("geometry",{primitive:"ring",radius_inner:10,radius_outer:12})
+        element.setAttribute("geometry",{primitive:"ring",radiusInner:0.9,radiusOuter:1})
+        element.setAttribute("material",{color:"black",opacity:1})
         element.setAttribute("position",position)
+        element.setAttribute("cursores",{})
         return element
     },
 
     createCardImage:function(item){
         var element = document.createElement("a-entity")
         element.setAttribute("visible",true)
-        element.setAttribute("geometry",{primitive:"circle",radius:10})
-        element.setAttribute("material",{src:"item.url"})
-
+        element.setAttribute("geometry",{primitive:"circle",radius:0.5})
+        element.setAttribute("material",{src:item.url})
         return element
-        
     },
+
+    createTitle:function(item,pos){
+        var element = document.createElement("a-entity")
+        element.setAttribute("text",{value:item.title,color:"black",width:20,font:"exo2bold"})
+        element.setAttribute("position",{x:pos.x+8,y:-2,z:pos.z})
+        return element
+    }
 
 
 });
